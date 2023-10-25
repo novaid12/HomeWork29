85203f90bf1ef12b52cb5ac8c995d894b431910b
novaid12 <spikulik8558@icloud.com>
2023-10-25 12:35:28 +0300

    Created project

---
+++ HomeWork29/HomeWork29/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork29
+//
+//  Created by  NovA on 25.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
